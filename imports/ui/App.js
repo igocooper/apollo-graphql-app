d64b4d18aeaa6553ee1310934098c3ecc3a0d03b
Oleg Lytvyn <igocooper@gmail.com>
2018-04-08 21:49:25 +0300

    lesson 7

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,12 +1,19 @@
-
 import React from 'react';
+import gql from 'graphql-tag' // to create graphql queries
+import { graphql } from 'react-apollo'
 
-const App = () => {
-    return (
-      <div>
-        <h1>Hello</h1>
-      </div>
-    )
-  }
+const App = (props) => (
+  <div>
+    <h1>{props.data.hi}</h1>
+    <p>I'm Oleg Lytvyn, I'm {props.data.age} old, and I'm learning development for a year and half already. 👍</p>
+  </div>
+)
 
-export default App;
+const hiQuery = gql`
+{
+  hi,
+  age
+}
+`;
+
+export default graphql(hiQuery)(App);
