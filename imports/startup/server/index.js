458792f143b4780eb9e8661a3e06692da2a6b170
Oleg Lytvyn <igocooper@gmail.com>
2018-04-08 13:54:17 +0300

    lesson-5

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,4 +1,31 @@
 import { createApolloServer } from 'meteor/apollo';
 import { makeExecutableSchema } from 'graphql-tools';
 
-createApolloServer({});
+const typeDefs = `
+    type Query {
+        hi: String,
+        age: Int,
+        greet: String
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hi() {
+            return 'Hi level up tuts'
+        },
+        greet() {
+            return 'Hi there iGoCooper'
+        },
+        age() {
+            return 26
+        }
+    }
+};
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+})
+
+createApolloServer({ schema });
